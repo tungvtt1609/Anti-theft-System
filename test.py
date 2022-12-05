75ebb32bab76dba0307930fa9e302966daf32c6b
Võ Thanh Tùng <parkhoon1609@gmail.com>
2022-12-05 17:04:09 +0700

    create test.py

--- test.py
+++ test.py
@@ -9,6 +9,8 @@ from subprocess import call
  
  
 PIR = 4 #khai báo chân cảm biến
+GPIO_ECHO = 24
+GPIO_TRIGGER = 18
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 25
@@ -16,6 +18,8 @@ camera.framerate = 25
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIR, GPIO.IN)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.OUT)
  
 motion = 0
 motionNew = 0
@@ -44,13 +48,13 @@ def main():
     global motion
     global motionNew
 
-    if GPIO.input(PIR) == 1:
+    if GPIO.input(GPIO_ECHO) == 1:
         print("Motion detected")
         motion = 1
         if motionNew != motion:
             motionNew = motion
             sendNotification(motion)
-    elif GPIO.input(PIR) == 0:
+    elif GPIO.input(GPIO_ECHO) == 0:
         print("No motion detected")
         motion = 0
         if motionNew != motion:
